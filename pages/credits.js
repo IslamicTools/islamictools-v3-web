43ef33b91cf8eda66ca32b629315a8809c026be2
Sego <sego@sego.app>
2021-10-02 18:24:13 +0200

    add base files

---
+++ pages/credits.js
@@ -0,0 +1,7 @@
+export default function Credits() {
+    return(
+        <>
+            <h2>Fajr Photo by <a href="https://unsplash.com/@davealmine">Dawid Zawiła</a> on <a href="https://unsplash.com/s/photos/sunrise">Unsplash</a></h2>
+        </>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="half-box">
+        <div className="prayer-card center">
+          <h3>Fajr is at<br/>
+        {"PLACEHOLDER"}</h3>
+        </div>
+      </div>
+    </>
+  )
+}
